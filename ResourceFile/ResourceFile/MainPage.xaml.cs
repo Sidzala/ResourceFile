d74d529a0b12f628f8a457b3e74731f65f05e9e9
Sid Zala <siddharthzala7@gmail.com>
2022-10-20 10:16:00 -0400

    Initial Project

---
+++ ResourceFile/ResourceFile/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Threading;
+using ResourceFile.Resources;
+using Xamarin.Forms;
+
+namespace ResourceFile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void ChangeLanguage_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (picker.SelectedItem != null)
+            {
+                var language = CultureInfo.GetCultures(CultureTypes.NeutralCultures).ToList().First(element => element.EnglishName.Contains(picker.SelectedItem.ToString())); ;
+                Thread.CurrentThread.CurrentUICulture = language;
+                AppResources.Culture = language;
+                new NavigationPage(new MyPage());
+            }
+        }
+    }
+}
+
---
+++ ResourceFile/ResourceFile/MyPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+using System.Linq;
+using System.Threading;
+using ResourceFile.Resources;
+using Xamarin.Forms;
+
+namespace ResourceFile
+{
+    public partial class MyPage : ContentPage
+    {
+        public MyPage()
+        {
+            InitializeComponent();
+        }
+
+        void ChangeLanguage1_Clicked(System.Object sender, System.EventArgs e)
+        {
+            if (picker.SelectedItem != null)
+            {
+                var language = CultureInfo.GetCultures(CultureTypes.NeutralCultures).ToList().First(element => element.EnglishName.Contains(picker.SelectedItem.ToString())); ;
+                Thread.CurrentThread.CurrentUICulture = language;
+                AppResources.Culture = language;
+                new NavigationPage(new MainPage());
+            }
+        }
+    }
+}
+
